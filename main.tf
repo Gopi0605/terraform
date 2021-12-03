671299d1f5b2fe96f90e796f29712dab8ac9d23f
gopimdvk <gopi.mdvk@gmail.com>
2021-12-03 05:59:31 +0530

    ec2 instance and keypain

--- main.tf
+++ main.tf
@@ -2,27 +2,26 @@ provider "aws" {
   region = "us-east-2"
 }
 
-
-variable cidr_blocks {
-  description = "Cidr_block "
-}
-
-
+variable cidr_blocks {}
+variable env {}
+variable instance_type {}
+variable "sshKey" {}
+variable "avail_zone" {}
 
 resource "aws_vpc" "vpc-dev" {
     cidr_block = var.cidr_blocks[0]
     tags = {
-        Name= "demo-vpc"
-        env = "dev"
+        Name= "${var.env}-vpc"
+
     }
 }
 
 resource "aws_subnet" "snet1-dev" {
   vpc_id = aws_vpc.vpc-dev.id
   cidr_block = var.cidr_blocks[1]
-  availability_zone = "us-east-2a"
+  availability_zone = var.avail_zone
   tags = {
-    "Name" = "demo-subnet1"
+    "Name" = "${var.env}-subnet1"
   }
 }
 
@@ -31,7 +30,7 @@ resource "aws_subnet" "snet2-dev" {
   cidr_block = var.cidr_blocks[2]
   availability_zone = "us-east-2b"
   tags = {
-    "Name" = "demo-subnet2"
+    "Name" = "${var.env}-subnet2"
   }
 }
 
@@ -41,7 +40,7 @@ output "vpc" {
 }
 
 output "subnet-1" {
-  value = aws_subnet.snet1-dev
+  value = aws_subnet.snet1-dev.id
 }
 
 /*
@@ -76,7 +75,7 @@ resource "aws_security_group" "sg-dev" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 } 
-
+*/
 
 resource "aws_default_security_group" "sg-dev"{
   vpc_id = aws_vpc.vpc-dev.id
@@ -108,7 +107,7 @@ resource "aws_default_security_group" "sg-dev"{
     cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
-    Name= "demo-sg"
+    Name= "${var.env}-sg"
   }
 }
 
@@ -116,7 +115,7 @@ resource "aws_default_security_group" "sg-dev"{
 resource "aws_internet_gateway" "ig-dev"{
 vpc_id = aws_vpc.vpc-dev.id
 tags={
-  Name = "demo-ig"
+  Name = "${var.env}-ig"
 }
 
 }
@@ -130,7 +129,7 @@ resource "aws_route_table" "it-dev"{
   tags ={
     Name = "demo-it"
   }
-} 
+} */
 
 resource "aws_default_route_table" "rt-dev"{
   default_route_table_id = aws_vpc.vpc-dev.main_route_table_id
@@ -139,7 +138,41 @@ resource "aws_default_route_table" "rt-dev"{
     gateway_id = aws_internet_gateway.ig-dev.id
   }
   tags = {
-    Name = "demo-rt"
+    Name = "${var.env}-rt"
   }
 }
-*/
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.ubuntu.id
+}
+resource "aws_key_pair" "dev-key" {
+  key_name = "master"
+  public_key = var.sshKey
+}
+
+resource "aws_instance" "dev-ec2" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = aws_key_pair.dev-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_default_security_group.sg-dev.id]
+  subnet_id = aws_subnet.snet1-dev.id
+  availability_zone = var.avail_zone
+  tags = {
+    "Name" = "${var.env}-server"
+  }
+
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,6 @@
-#cidr_blocks = ["10.0.0.0/16","10.0.0.0/24","10.0.10.0/24"]
+cidr_blocks = ["10.0.0.0/16","10.0.0.0/24","10.0.10.0/24"]
+env = "dev"
+instance_type = "t2.micro"
+
+sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDvH75gQJA7aOOQya0NJ56ZYN3IJgaiYp6fN3yJ9DRpfBxCCliR07jAfYJ9Y30i8muiy+UEy8CVnTK3JnW8KyY1AsGyOCmaOuGfxxCkWF1u1FH8lqwRUiZHibULKu9jyTcXhBsNcU+DO50/Yo3+a97vqT+nnMKzHZPr/Dk7uDWZGLzuwbNGQ6Mk/ZPMls8LZK5IRaP1Lr5Ubnd/VUdO59zRYr6kQsRcKJrLbTnGnU54SDzCgd5aXb859hrDwr7iF+Vf2C/IlfXeFxRWHUEFEwYDqlOeOueQnFROSxjlCqfd3rbH/UkUkLWacXyBjdWrhak/FH7LkRJdY9i6hHygWwIeVS+eLEmrsaatJTslzvBNGhCcInrD57OmDO5VCcQKsg2lF/qLRBbW4SPHn4nXxk5nPNab2/gbtHAKLQEQHe4yMnCdwfXypWcyKCpI7MzDTtCi6AD0Dg/ZrlC5IfjWGgwGMOOsT7bsiGSsIhhpPAaaneLf0nwhvMKo5W6JLJ5PS2U= gopi@gopi"
+avail_zone = "us-east-2a"
